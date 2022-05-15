4e9de07a6ddc0035507f172e9c348858dcfb9f08
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-15 23:25:40 +0100

    fixed newsletter-archive module target

--- ch09/meadowlark.js
+++ ch09/meadowlark.js
@@ -47,7 +47,7 @@ app.get('/', handlers.home)
 app.get('/newsletter-signup', handlers.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
-app.get('/newsletter-archive', handlers.newsletterSignupThankYou)
+app.get('/newsletter-archive', handlers.newsletterArchive)
 
 app.use(handlers.notFound)
 app.use(handlers.serverError)
